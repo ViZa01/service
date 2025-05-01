57ec898a97712f78ebce0922ffaf6d6e0f9c99aa
ViZa01 <103385911+ViZa01@users.noreply.github.com>
2025-05-01 13:17:45 +0300

    was added js scripts

---
+++ src/js/test.js
@@ -0,0 +1,14 @@
+// Exercise_1
+// let a = 32;
+// let b = 23;
+// let c = a + b;
+// console.log(" Result is " + a + b + "is" + c);
+
+// Exercise_2
+// for (let a = 0; a <= 10; a++) {
+//   if (a % 2 === 0) {
+//     console.log(a);
+//   }
+// }
+
+// Exercise_3
